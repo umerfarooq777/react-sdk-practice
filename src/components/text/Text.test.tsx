901cf2867fdb4264d701543de1cc66b6985adbb0
umerfarooq777 <umerfarooq03100276796@gmail.com>
2023-11-13 20:42:46 +0500

    "npm run test" to test components

---
+++ src/components/text/Text.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Text from "./Text";
+
+describe("Text", () => {
+    test("renders the Text component", () => {
+        render(<Text text="Hello world! " />);
+    });
+});
