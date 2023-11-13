29cdc0146ba74fd61b18f478262e7ac91afc2ea6
umerfarooq777 <umerfarooq03100276796@gmail.com>
2023-11-13 16:43:08 +0500

    "npm run rollup" built successfully

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-import plugin from "rollup-plugin-dts";
+import { dts } from "rollup-plugin-dts";
 const packageJson = require("./package.json");
 
 
